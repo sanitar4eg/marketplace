91dd4032d569c6dfb3e95eb0b7915d2d43948916
Denis <sanitar.92@gmail.com>
2016-08-27 19:44:37 +0300

    Initial commit

---
+++ src/main/java/edu/learn/market/MarketplaceApp.java
@@ -0,0 +1,42 @@
+package edu.learn.market;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+import javax.inject.Inject;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@ComponentScan
+@EnableAutoConfiguration
+public class MarketplaceApp {
+
+    private static final Logger log = LoggerFactory.getLogger(MarketplaceApp.class);
+
+    @Inject
+    private Environment env;
+
+    /**
+     * Main method, used to run the application.
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws UnknownHostException {
+        SpringApplication app = new SpringApplication(MarketplaceApp.class);
+        Environment env = app.run(args).getEnvironment();
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\thttp://127.0.0.1:{}\n\t" +
+                        "External: \thttp://{}:{}\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                env.getProperty("server.port"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"));
+
+    }
+
+}
