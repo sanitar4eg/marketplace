56c2dda318ab5193a291c9f1088e1c3f3f385ed0
Denis <sanitar.92@gmail.com>
2016-09-14 01:33:36 +0300

    Adding logic for login page

--- src/main/java/edu/learn/market/web/LoginController.java
+++ src/main/java/edu/learn/market/web/LoginController.java
@@ -1,8 +1,14 @@
 package edu.learn.market.web;
 
+import edu.learn.market.domain.UserMP;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.actuate.info.InfoPropertiesInfoContributor;
+import org.springframework.cglib.core.CollectionUtils;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -11,9 +17,20 @@ public class LoginController {
 
     private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(LoginController.class);
 
-    @GetMapping("/")
-    public String login(Map<String, Object> model) {
-        LOG.info("Login get request");
+    @GetMapping("/login")
+    public String getLogin(Model model) {
+        LOG.debug("Login get request");
+        model.addAttribute("userMP", new UserMP());
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String postLogin(@ModelAttribute @Validated UserMP userMP, BindingResult result, Model model) {
+        LOG.debug("Login post request: " + userMP);
+        if (result.hasErrors()) {
+            LOG.warn("Errors", result);
+            model.addAttribute("userMP", userMP);
+        }
         return "login";
     }
 }
--- src/main/webapp/WEB-INF/jsp/login.jsp
+++ src/main/webapp/WEB-INF/jsp/login.jsp
@@ -1,1 +1,27 @@
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ page language="java" pageEncoding="UTF-8" %>
+<%@ page contentType="text/html;charset=UTF-8" %>
 <%@ include file="header.jsp" %>
+
+<body>
+<div class="container">
+
+    <form:form class="form-email" method="post" modelAttribute="userMP">
+
+        <input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}"/>
+
+        <h2>Пожалуйста авторизуйтесь</h2>
+        <label for="inputEmail" class="sr-only">Email</label>
+        <form:input type="email" id="inputEmail" path="email" class="form-control" placeholder="Email" required="true"
+                    autofocus="true"/>
+        <form:errors path="email"/>
+        <label for="inputPassword" class="sr-only">Пароль</label>
+        <form:input type="password" id="inputPassword" path="password" class="form-control" placeholder="Пароль"
+                    required="true"/>
+        <form:errors path="password"/>
+        <form:button class="btn btn-lg btn-primary btn-block" type="submit" id="email">Войти</form:button>
+    </form:form>
+
+</div> <!-- /container -->
+
+</body>
