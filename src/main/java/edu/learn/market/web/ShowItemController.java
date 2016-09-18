87d67629fd9043f1c70708f0e221e362304e88c9
Denis <sanitar.92@gmail.com>
2016-09-18 21:22:47 +0300

    ShowItemController was updated

--- src/main/java/edu/learn/market/web/ShowItemController.java
+++ src/main/java/edu/learn/market/web/ShowItemController.java
@@ -2,9 +2,8 @@ package edu.learn.market.web;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.Map;
 
 @Controller
 public class ShowItemController {
@@ -12,8 +11,8 @@ public class ShowItemController {
     private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(ShowItemController.class);
 
     @GetMapping("/show-item")
-    public String showItem(Map<String, Object> model) {
+    public String showItem(Model model) {
 
-        return "showItem";
+        return "show-item";
     }
 }
