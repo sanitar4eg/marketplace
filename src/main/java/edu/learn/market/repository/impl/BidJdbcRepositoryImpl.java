b0a6a3b5b8570311cf663d75db694f37ea59c6cd
Denis <sanitar.92@gmail.com>
2016-11-16 16:44:50 +0300

    BidJdbcRepositoryImpl.java was moved to Spring templates

--- src/main/java/edu/learn/market/repository/impl/BidJdbcRepositoryImpl.java
+++ src/main/java/edu/learn/market/repository/impl/BidJdbcRepositoryImpl.java
@@ -6,66 +6,80 @@ import edu.learn.market.domain.UserMP;
 import edu.learn.market.repository.BidRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.SqlParameterValue;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.Assert;
 
-import javax.persistence.EntityManager;
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Optional;
+import java.sql.Types;
+import java.util.*;
 
 @Repository
+@SuppressWarnings("unused")
 public class BidJdbcRepositoryImpl implements BidRepository {
 
     private static final Logger LOG = LoggerFactory.getLogger(BidJdbcRepositoryImpl.class);
 
-    private static final String INSERT_QUERY = "INSERT INTO bids"
+    private static final String UPSERT_QUERY = "INSERT INTO bids"
             + "(id, bid, item_id, usermp_id) VALUES"
-            + "(?,?,?,?)";
+            + "(:id, :bid, :item_id, :usermp_id) ON CONFLICT (id) DO "
+            + "UPDATE SET bid = :bid, item_id = :item_id, usermp_id = :usermp_id";
 
     private static final String SELECT_ALL_QUERY = "SELECT "
             + "id, bid, item_id, usermp_id FROM bids";
 
+    private static final String SELECT_ONE_QUERY = "SELECT "
+            + "id, bid, item_id, usermp_id FROM bids "
+            + "WHERE id = ?";
+
     private static final String DELETE_QUERY = "DELETE FROM bids "
             + "where id = ?";
 
-    private static final String SELECT_NEXT_SEQUENCE = "select nextval ('hibernate_sequence')";
+    private static final String SELECT_NEXT_SEQUENCE = "SELECT nextval ('hibernate_sequence')";
 
-    private EntityManager entityManager;
+    private NamedParameterJdbcTemplate namedTemplate;
 
-    private NamedParameterJdbcTemplate template;
+    private JdbcTemplate template;
 
-    private DataSource dataSource;
+    private BidsResultSetExtractor setExtractor;
 
-    public BidJdbcRepositoryImpl(EntityManager entityManager, DataSource dataSource) {
-        this.entityManager = entityManager;
-        template = new NamedParameterJdbcTemplate(dataSource);
-        this.dataSource = dataSource;
+    public BidJdbcRepositoryImpl(DataSource dataSource) {
+        this.namedTemplate = new NamedParameterJdbcTemplate(dataSource);
+        this.template = new JdbcTemplate(dataSource);
+        this.setExtractor = new BidsResultSetExtractor();
+    }
+
+    private class BidsResultSetExtractor implements ResultSetExtractor<List<Bid>> {
+
+        @Override
+        public List<Bid> extractData(ResultSet rs) throws SQLException, DataAccessException {
+            List<Bid> list = new LinkedList<>();
+            while (rs.next()) {
+                Bid bid = new Bid();
+                bid.id(rs.getLong(1))
+                        .bid(rs.getLong(2));
+                Optional.ofNullable(rs.getLong(3))
+                        .ifPresent(itemId -> bid.item(new Item().id(itemId)));
+                Optional.ofNullable(rs.getLong(4))
+                        .ifPresent(userId -> bid.userMP(new UserMP().id(userId)));
+                list.add(bid);
+            }
+            return list;
+        }
     }
 
     @Override
     public Bid save(Bid bid) {
         Assert.notNull(bid);
-        Connection connection = DataSourceUtils.getConnection(dataSource);
-        try (
-                PreparedStatement statement = connection.prepareStatement(INSERT_QUERY)
-        ) {
-            Long id = bindParameters(bid, statement, connection);
-            LOG.debug("Query is ready: " + INSERT_QUERY);
-            statement.execute();
-            return findOne(id);
-        } catch (Exception e) {
-            LOG.error("Error when saving Bid", e);
-        }
-        return null;
+        LOG.debug("Query is ready: " + UPSERT_QUERY);
+        namedTemplate.update(UPSERT_QUERY, getParamMap(bid));
+        return bid;
     }
 
     @Override
@@ -75,77 +89,49 @@ public class BidJdbcRepositoryImpl implements BidRepository {
 
     @Override
     public Bid findOne(Long id) {
-        return null;
+        Assert.notNull(id);
+        LOG.debug("Query is ready: " + SELECT_ONE_QUERY);
+        List<Bid> list = template.query(SELECT_ONE_QUERY, new Long[]{id}, setExtractor);
+        if (list.size() > 0) {
+            return list.get(0);
+        } else {
+            return null;
+        }
     }
 
     @Override
     public List<Bid> findAll() {
-        Connection connection = DataSourceUtils.getConnection(dataSource);
-        try (
-                PreparedStatement statement = connection.prepareStatement(SELECT_ALL_QUERY)
-        ) {
-            LOG.debug("Query is ready: " + SELECT_ALL_QUERY);
-            ResultSet resultSet = statement.executeQuery();
-            return extractResults(resultSet);
-        } catch (Exception e) {
-            LOG.error("Error when saving Bid", e);
-        }
-        return Collections.emptyList();
+        LOG.debug("Query is ready: " + SELECT_ALL_QUERY);
+        return template.query(SELECT_ALL_QUERY, setExtractor);
     }
 
     @Override
     public void delete(Long id) {
         Assert.notNull(id);
-        Connection connection = DataSourceUtils.getConnection(dataSource);
-        try (
-                PreparedStatement statement = connection.prepareStatement(DELETE_QUERY)
-        ) {
-            statement.setLong(1, id);
-            LOG.debug("Query is ready: " + DELETE_QUERY);
-            statement.execute();
-        } catch (Exception e) {
-            LOG.error("Error when saving Bid", e);
-        }
+        LOG.debug("Query is ready: " + DELETE_QUERY);
+        template.update(DELETE_QUERY, id);
     }
 
-    private Long getNextId(Connection connection) throws SQLException {
-        try (
-                PreparedStatement preparedStatement =
-                        connection.prepareStatement(SELECT_NEXT_SEQUENCE)) {
-            LOG.debug("Query is ready: " + SELECT_NEXT_SEQUENCE);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            resultSet.next();
-            return resultSet.getLong(1);
-        } catch (SQLException e) {
-            LOG.error("Error when getting ID", e);
-            throw e;
-        }
+    private Long getNextId() {
+        LOG.debug("Query is ready: " + SELECT_NEXT_SEQUENCE);
+        return template.queryForObject(SELECT_NEXT_SEQUENCE, Long.class);
     }
 
-    private Long bindParameters(Bid bid, PreparedStatement statement, Connection connection) throws SQLException {
+    private Map<String, ?> getParamMap(Bid bid) {
         if (bid.getId() == null) {
-            bid.id(getNextId(connection));
+            bid.id(getNextId());
         }
-        statement.setLong(1, bid.getId());
-        statement.setLong(2, bid.getBid());
-        statement.setLong(3, bid.getItem().getId());
-        statement.setLong(4, bid.getUserMP().getId());
-        return bid.getId();
+        Map<String, SqlParameterValue> map = new HashMap<>();
+        map.put("id", new SqlParameterValue(Types.LONGVARBINARY, bid.getId()));
+        map.put("bid", new SqlParameterValue(Types.LONGVARBINARY, bid.getBid()));
+        map.put("item_id",
+                new SqlParameterValue(Types.LONGVARBINARY, Optional.ofNullable(bid.getItem())
+                        .map(Item::getId)
+                        .orElse(null)));
+        map.put("usermp_id",
+                new SqlParameterValue(Types.LONGVARBINARY, Optional.ofNullable(bid.getUserMP())
+                        .map(UserMP::getId)
+                        .orElse(null)));
+        return map;
     }
-
-    private List<Bid> extractResults(ResultSet resultSet) throws SQLException {
-        List<Bid> list = new LinkedList<>();
-        while (resultSet.next()) {
-            Bid bid = new Bid();
-            bid.id(resultSet.getLong(1))
-                    .bid(resultSet.getLong(2));
-            Optional.ofNullable(resultSet.getLong(3))
-                    .ifPresent(itemId -> bid.item(new Item().id(itemId)));
-            Optional.ofNullable(resultSet.getLong(4))
-                    .ifPresent(userId -> bid.userMP(new UserMP().id(userId)));
-            list.add(bid);
-        }
-        return list;
-    }
-
 }
