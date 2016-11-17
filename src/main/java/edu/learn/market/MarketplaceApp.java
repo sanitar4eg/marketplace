38b38a1be3f3841086ff2c723173f79c94e7b829
Denis <sanitar.92@gmail.com>
2016-11-17 22:07:47 +0300

    Updated annotation for spring boot application

--- src/main/java/edu/learn/market/MarketplaceApp.java
+++ src/main/java/edu/learn/market/MarketplaceApp.java
@@ -3,16 +3,14 @@ package edu.learn.market;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
 
 import javax.inject.Inject;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 public class MarketplaceApp {
 
     private static final Logger LOG = LoggerFactory.getLogger(MarketplaceApp.class);
